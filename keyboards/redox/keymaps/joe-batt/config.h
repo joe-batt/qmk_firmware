a507e0bbbef0afda4b1a9b22db48a4c4fcd9587e
Johannes Battenberg <battenberg@rz.uni-frankfurt.de>
2021-12-23 21:41:35 +0100

    added my personal keymap

--- keyboards/redox/keymaps/joe-batt/config.h
+++ keyboards/redox/keymaps/joe-batt/config.h
@@ -17,14 +17,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 // #define USE_I2C
@@ -41,28 +33,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_LAYERS
-#define COMBO_COUNT 9
-//#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
-//#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-//#define RETRO_TAPPING
-//#define RETRO_TAPPING_PER_KEY
-#define NKRO_ENABLE
-#define TAPPING_TOGGLE 3
-#define TAPPING_TERM 190
-#define DYNAMIC_MACRO_NO_NESTING
-//#define MK_3_SPEED
-//#define MK_C_OFFSET_0 8
-//#define MK_C_INTERVAL_0 16
-//#define MK_C_OFFSET_1 12
-//#define MK_C_INTERVAL_1 16
-//#define MK_C_OFFSET_2 16
-//#define MK_C_INTERVAL_2 16
-//
-#define MK_KINETIC_SPEED
-#define RGBLIGHT_LAYER_BLINK
-#define MOUSEKEY_INTERVAL 8
-#define MOUSEKEY_MOVE_DELTA 1
-#define MOUSEKEY_INITIAL_SPEED 2
-#define MOUSEKEY_BASE_SPEED 32
-#define USB_POLLING_INTERVAL_MS 2
+#define COMBO_COUNT 1
